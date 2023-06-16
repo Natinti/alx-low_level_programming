fbb78e59be5e4fd76b0f680a4dacfc2183f310f2
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-16 10:35:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - Entry Point 
+ * main - Entry Point
  * A program to print line of code to show standard error
  *
  * Return: 1 (Success)
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 }
