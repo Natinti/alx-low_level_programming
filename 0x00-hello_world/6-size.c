e5845192f766be089c3f14aaa293c37a4f26f22a
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-15 19:44:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
+#include <stdio>
 
 /**
-*main - Entry Point
-*A program that prints the various type on the computer
-*
-* Return :0(Success)
-*/
+ * main- Entry Point
+ * A program to run the computer size of
+ *
+ * Return:0 (success)
+ */
+
 int main(void)
 {
 printf("Size of char:%zu byte(s)\n", sizeof(char));
 printf("Size of int:%zu byte(s)\n", sizeof(int));
-printf("Size of short:%zu byte(s)\n", sizeof(short));
-printf("Size of long:%zu byte(s)\n", sizeof(long));
-printf("Size of long long:%zu byte(s)\n", sizeof(long long));
+printf("Size of long int:%zu byte(s)\n", sizeof(long int));
+printf("Size of long long int:%zu byte(s)\n", sizeof(long long int));
 printf("Size of float:%zu byte(s)\n", sizeof(float));
-printf("Size of double: %zu byte(s)\n", sizeof(double));
-printf("Size of long double: %zu byte(s)\n", sizeof(long double));
 return (0);
 }
