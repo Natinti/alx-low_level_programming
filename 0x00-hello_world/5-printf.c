b0964c761baa094230264ccc5890b53ff62454db
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-15 17:46:10 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry Point
- * A program to print a line in printf function
- * Return : Always 0 (Success)
- **/
-
+ * main - A program that print a line using printf function
+ *
+ * Return : 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
