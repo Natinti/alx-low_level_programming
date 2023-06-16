f3937b25f0e12329360fc6d5e4c73a548ed0e749
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-16 09:38:22 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * A program that run the computer sizeof type
+ * A Program to run the line for the function for a sizeof computer
  *
- * Return:0 (Success)
+ * Return: 0 (Success)
+ *
  */
 
 int main(void)
@@ -13,10 +14,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu byte(s)\n"' (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu  byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(e));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 
 return (0);
+}
