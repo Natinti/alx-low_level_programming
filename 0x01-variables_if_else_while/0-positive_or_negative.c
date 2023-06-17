81a6a4fa2821613c7f9fb4cc98a2321bb2e0f83b
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-17 14:18:46 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ * A program to run the if_else statement function
+ *
+ * Return: 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there*/
+printf("The number %d is ", n)
+if (n > 0)
+{
+printf("positive");
+}
+else if (n < 0)
+{
+printf("negative");
+}
+else
+{
+printf("zero");
+}
+printf("\n");
+
+return (0);
+}
