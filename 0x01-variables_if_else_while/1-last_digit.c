30545599d6a9d9fa1a751b1676b7655359782d55
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-18 14:41:56 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@
 
 int main(void)
 {
-	int n,lastDigit;
+	int n, lastDigit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
@@ -26,14 +26,15 @@ if (lastDigit > 5)
 {
 	printf("and is greater than 5\n");
 }
-else if (lastDigit == 0)
+else if (lastDigit < 6 && lastDigit != 0)
 {
-	printf("and is 0\n");
+	printf("and is less than 6 and not 0\n");
 
 }
 else
 {
-	printf("and is less than 6 and not 0\n");
+	printf("and is 0\n");
+
 }
 
 return (0);
