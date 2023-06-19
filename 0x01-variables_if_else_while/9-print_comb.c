3a3a39016c28a00760279a1e2a505e2ea22b1865
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-19 15:20:58 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * A program that print combination with putchar function
+ *
+ * Return:0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int  comb;
+/*Print combination using Putchar*/
+
+for (comb = 0; comb < 10; comb++)
+{
+	putchar('0' + comb);
+	if (comb < 10)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
+
