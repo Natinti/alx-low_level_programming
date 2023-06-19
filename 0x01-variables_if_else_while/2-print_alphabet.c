8e85e8a3d0977aa046919d97ca206986f0437224
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-19 10:56:27 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * A program to run line of ocde for putchar function
+ *
+ * Return:0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	/*Printing lower alphabets in putchar*/
+	for (alpha ='a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	/*Using putchar to print newline*/
+
+	putchar('\n');
+
+	return (0);
+}
