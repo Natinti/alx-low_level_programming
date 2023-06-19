bfa9f419b1152ce6c3a723226f04b60aaa25f990
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-19 10:21:52 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,20 +24,20 @@ lastDigit = n % 10;
 if (lastDigit > 5)
 {
 
-printf("Last Digit of %d is %d and is greater than 5\n", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 
 }
 else if (lastDigit == 0)
 {
 
-printf("Last Digit of %d is %dand is 0\n", n, lastDigit);
+printf("Last digit of %d is %dand is 0\n", n, lastDigit);
 
 }
 
 else if (lastDigit < 6 && lastDigit != 0)
 {
 
-printf("Last Digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 
 }
 
