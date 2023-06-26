f84ee7c895ec5c9caa0573dcb18c8118b367c769
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-26 09:56:15 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_rev - prints a string in reverse followed by new line
+ *@s: string
+ *Return: 0
+ *
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*S);
+		s--;
+	}
+	_putchar ('\n');
+
+}
