6153ad193928eb9640b33329acde44b8bf3865a5
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-26 11:01:35 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts - function to print one character out of two
+ * Starting with the first one
+ * @str : input
+ * Return: print
+ */
+
+void puts(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar ('\n');
+}
