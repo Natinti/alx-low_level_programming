b886a9971cf5151ba4747478a7d2fb8967dd084d
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-28 07:46:33 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * int_islower - a program that shows 1 if input is a 
+ * lowercase character. Other cases shows 0
+ *
+ * @c: The character is ASCII code 
+ *
+ * Return: 1 for lowercase character. 0 for rest
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
