0716af69eb81d5b9e5e978b4d0aa25153992e229
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-28 06:23:59 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet in lowercase 
+ * print_alphabet - prints alphabet in lowercase
  *
  * Return: Always 0
  */
@@ -9,8 +9,10 @@
 void print_alphabet(void)
 {
 	char alpha;
+
 	for (alpha = 'a'; alpha >= 'z'; alpha++)
 	{
 		_putchar(alpha);
+
 	}
 	_putchar('\n');
