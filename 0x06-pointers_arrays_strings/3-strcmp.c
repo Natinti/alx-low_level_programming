188207da937376bf4608c1d1e9a8fd8be8fa2c36
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-29 11:31:22 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if(s1[i] != s2[i])
+		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
@@ -22,3 +23,4 @@ int _strcmp(char *s1, char *s2)
 	}
 
 	return (0);
+}
