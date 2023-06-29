78aafbe59171535cb23f05d00337b545a24e88f7
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-29 14:35:29 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - This function capitalizes all owrds of a string
+ * @str: The string that is to be capitalized
+ *
+ * Return: Pointer to the changed string
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == '' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ','  ||
+				str[index - 1] == ';'  ||
+				str[index - 1] == '.'  ||
+				str[index - 1] == '!'  ||
+				str[index - 1] == '?'  ||
+				str[index - 1] == '''' ||
+				str[index - 1] == '('  ||
+				str[index - 1] == ')'  ||
+				str[index - 1] == '{'  ||
+				str[index - 1] == '}'  ||
+				index == 0)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+
+}
+
