36315ec72c0ea598bc90157c0e9022879d2dae70
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-06-29 14:40:12 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
  * Return: Pointer to the changed string
  */
 
-char *cap_string(char *str)
+char *cap_string(char *)
 {
 	int index = 0;
 
@@ -24,7 +24,7 @@ char *cap_string(char *str)
 				str[index - 1] == '.'  ||
 				str[index - 1] == '!'  ||
 				str[index - 1] == '?'  ||
-				str[index - 1] == '''' ||
+				str[index - 1] == '"' ||
 				str[index - 1] == '('  ||
 				str[index - 1] == ')'  ||
 				str[index - 1] == '{'  ||
