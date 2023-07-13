425ac993affafc063e824a80be43df4d0889b251
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-07-13 04:01:34 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memeory using malloc function
+ * @b: number of bytes that is to be allocated
+ *
+ * Return: a pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
