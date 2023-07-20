277379ae4600ac0bb611ae7e1a0f05256e007039
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-07-20 16:28:28 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,11 +22,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(ap, int);
-		printf("%d ", num);
+		printf("%d", num);
 
-		if (i < n - 1 && separator != NULL)
+		if (i < (n - 1) && separator != NULL)
 		{
-			printf("%s ", separator);
+			printf("%s", separator);
 		}
 	}
 
