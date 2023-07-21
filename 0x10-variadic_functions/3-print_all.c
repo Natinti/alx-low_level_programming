27964ace270cdbb6cd24f6459ace419310500a7f
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-07-20 22:26:30 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,40 +10,41 @@ void print_all(const char * const format, ...)
 	char ch, current_format;
 	int num, format_index = 0;
 	float fnum;
-	char *str;
+	char *str, *sep = "";
 	va_list list;
 
 	va_start(list, format);
 
-	while ((current_format = format[format_index++]) != '\0')
+	while ((current_format = format[format_index]))
 	{
 		if (current_format == 'c')
 		{
 			ch = va_arg(list, int);
-			printf("%c", ch);
+			printf("%s%c", sep,  ch);
 		}
 		else if (current_format == 'i')
 		{
 			num = va_arg(list, int);
-			printf("%d", num);
+			printf("%s%d", sep,  num);
 		}
 		else if (current_format == 'f')
 		{
 			fnum = va_arg(list, double);
-			printf("%f", fnum);
+			printf("%s%f", sep,  fnum);
 		}
 		else if (current_format == 's')
 		{
 			str = va_arg(list, char*);
 			if (str != NULL)
 			{
-				printf("%s", str);
+				printf("%s%s", sep,  str);
 			}
 			else
 			{
 				printf("(nil)");
 			}
 		}
-	}
+		sep = ",";
+		format_index++; }
 	va_end(list);
 	printf("\n"); }
