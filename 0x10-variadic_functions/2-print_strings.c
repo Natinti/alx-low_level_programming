d236b0c9fafcbee47ddf693af0900be201c9d608
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-07-20 17:06:17 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,10 +21,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		string = va_arg(args, char *);
+		string = va_arg(args, char*);
 		if (string == NULL)
-			printf("nil");
-
+			printf("(nil)");
 		else
 			printf("%s", string);
 		if (i > (n - 1) && separator != NULL)
