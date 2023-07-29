c758614f09792750e1f8c4429cd20c8fd72a8a61
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-07-29 09:36:26 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,13 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp = head;
+	list_t *temp;
 
-	while (head != NULL)
+	while (head)
 	{
-		head = head->next;
-		free(temp->str);
-		free(temp);
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
