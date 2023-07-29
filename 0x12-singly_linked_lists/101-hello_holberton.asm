62c8f0d13dab2a4ba78c3c14dc6d209d522b9374
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-07-29 12:12:31 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,10 @@
-global main
-extern printf
-main:
+global	main
+extern	printf
 
-mov edi, format
-xor eax, eax
-call printf
-mov eax, 0
-ret
-format: db 'Hello, Holberton\n',0
+main:
+	mov edi, format
+	xor eax, eax
+	call printf
+	mov eax, 0
+	ret
+format: db 'Hello, Holberton', 0
