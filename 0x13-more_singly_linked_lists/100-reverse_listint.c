3581b416f11b25550e42ec43cc5eeb10867bfd02
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-08-01 10:25:56 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,17 +10,16 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *current = *head;
 	listint_t *next = NULL;
 
-	while (current != NULL)
+	while (*head != NULL)
 	{
-		next = current->next;
+		next = (*head)->next;
 
-		current->next = prev;
+		(*head)->next = prev;
 
-		prev = current;
-		current = next;
+		prev = *head;
+		*head = next;
 	}
 	*head = prev;
 	return (*head);
