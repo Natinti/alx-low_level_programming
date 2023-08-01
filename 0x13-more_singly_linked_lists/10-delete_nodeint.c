8abe4cdbd216244ab34b831d9ccef6d8f3b934cc
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-08-01 09:52:46 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 
-	if (current == NULL || current->next == NULL)
+	if (temp == NULL || temp->next == NULL)
 	{
 		return (-1);
 	}
