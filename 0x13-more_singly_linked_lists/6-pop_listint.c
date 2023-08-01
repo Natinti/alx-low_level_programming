e2f05b3beba4fac15ac69c69ace30cea13a04443
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-08-01 08:06:20 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,7 +13,7 @@ int pop_listint(listint_t **head)
 	listint_t *temp = *head;
 	int count;
 
-	if (head || *head == NULL)
+	if (*head == NULL)
 	{
 		return (0);
 	}
@@ -21,7 +21,5 @@ int pop_listint(listint_t **head)
 	count = (*head)->n;
 	*head = (*head)->next;
 	free(temp);
-	*head = temp;
-
 	return (count);
 }
