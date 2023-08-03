a42f2aeac189ef9ef9d17cc83dc8825a9c70a077
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-08-03 16:16:47 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -15,7 +15,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int count = 0;
 	unsigned long int result = n ^ m;
 
-	while(result)
+	while (result)
 	{
 		result &= (result - 1);
 		count++;
