d7225e06e4fb136d0165c490fd7a8c58badece50
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2023-08-10 11:04:52 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,25 +15,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	FILE *file;
 	char *buffer;
 	ssize_t r;
+	ssize_t w;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
 	file = fopen(filename, "r");
 	if (file == NULL)
 	{
 		return (0);
 	}
-
 	buffer = malloc(sizeof(char) + (letters + 1));
 	if (buffer == NULL)
 	{
 		fclose(file);
 		return (0);
 	}
-
 	r = fread(buffer, 1, letters, file);
 	if (r <= 0)
 	{
@@ -43,6 +41,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	buffer[r] = '\0';
 	printf("%s", buffer);
+	w = fwrite(buffer, 1, r, stdout);
+	if (w != r)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
 	fclose(file);
 	free(buffer);
 	return (r);
